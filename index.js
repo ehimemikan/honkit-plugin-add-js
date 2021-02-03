c68753661e282e63f5ba15e3b80e731bd905f6b9
ehimemikan <ehimemikan100@gmail.com>
2021-02-03 20:20:45 +0900

    release honkit-plugin-add-js

--- index.js
+++ index.js
@@ -2,29 +2,27 @@ const path = require('path');
 const fs = require('fs');
 const tmp = require('tmp');
 const crypto = require('crypto');
-let jsFiles = [];
+var jsFiles = [];
 
 module.exports = {
-    hooks: {
-        "init": () => {
-            const book = this;
-            const tmpobj = tmp.dirSync({ mode: 0755 });
-            const customJs = this.config.get('pluginsConfig.add-js.js', []);
-            if (!book.isLanguageBook()) {
-                customJs.forEach((file) => {
-                    const origin = book.resolve(file);
-                    const filename = `${crypto.randomBytes(N).toString('base64').substring(0, N)}-${path.basename(origin)}`;
-                    const output = path.resolve(tmpobj.name, filename);
-                    const content = fs.readFileSync(origin);
-                    fs.writeFileSync(output, content);
+    website: function() {
+        const book = this;
+        const tmpobj = tmp.dirSync({ mode: 0755 });
+        const customJs = book.config.get('pluginsConfig.add-js.js', []);
+        if (!book.isLanguageBook()) {
+            customJs.forEach((file) => {
+                const origin = book.resolve(file);
+                const filename = `${crypto.randomBytes(16).toString('base64').substring(0, 16)}-${path.basename(origin)}`;
+                const output = path.resolve(tmpobj.name, filename);
+                const content = fs.readFileSync(origin);
+                fs.writeFileSync(output, content);
 
-                    jsFiles.push(filename);
-                });
-            }
-            return {
-                assets: tmpobj.name,
-                js: jsfiles
-            };
+                jsFiles.push(filename);
+            });
+        }
+        return {
+            assets: tmpobj.name,
+            js: jsFiles,
         }
     }
 }
